6dda8418c26c652657dc8fcca09fdf33a14147e7
arleyribeiro <arley.sribeiro@gmail.com>
2016-05-05 13:54:26 -0300

    Exercicio_4

--- js/my_first_io_a.js
+++ js/my_first_io_a.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-fs.readFile('js/my_first_io.js',"utf8", function(err, data) {
+fs.readFile('my_first_io.js',"utf8", function(err, data) {
   if(err)
     throw err;
     console.log(data);
